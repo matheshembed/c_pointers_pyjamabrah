181bbc0a3600d6bf335cb6b277a7ae71ba09dd58
matheshembed <matheshvarma25@gmail.com>
2025-07-22 20:52:08 +0530

    Memory Model

--- Source_Codes/13.c
+++ Source_Codes/13.c
@@ -1,1 +1,5 @@
-/*S5L1. What is a function pointer? | Understanding C - Pointers*/
+/*S4L4. Memory model and pointer to data | Understanding C - Pointers*/
+/* Explanation of memory model is done here*/
+
+// vid link
+//https://www.youtube.com/watch?v=uBdsS43iwss&list=PLTbERlX_4R01jACkwwAtD5NR-NFZciEHC&index=16&pp=iAQB
