bb03a7ac54636c4343ef67b6021df0e04a711254
matheshembed <matheshvarma25@gmail.com>
2025-07-30 20:51:13 +0530

    Code Upload

--- Source_Codes/14.c
+++ Source_Codes/14.c
@@ -1,1 +1,17 @@
+/*S5L1. What is a function pointer? | Understanding C - Pointers*/
 
+#include <stdio.h>
+{
+    // A general overview is given
+    //https://youtu.be/ZYDY2B50pws?si=FP8y0LQdW26nKrCv
+
+    /*
+    In case of dereferencing a function pointer, the program counter value
+    is changed to the address of the fuction pointer was holding
+    and because of that the instruction that the program counter fetch changes
+
+    They key point here is the program counter is manipulated some how and the function pointer fetches instruction from the program counter which is pointing to some location.
+    
+     
+    */
+}
